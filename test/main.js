7a5d2c24d8008ca3d6428353c1d9cf4342d36007
Michael Scholtz <michael.scholtz@outlook.com>
2015-06-28 10:17:14 +0200

    Added nocks for HTTP requests.

--- test/main.js
+++ test/main.js
@@ -2,6 +2,15 @@
 
 var SteamRepAPI = require('../');
 var expect = require('chai').expect;
+var nock = require('nock');
+
+var isScammerFalse = nock("http://steamrep.com")
+                     .get("/id2rep.php?steamID32=STEAM_0:0:84901")
+                     .reply(200, "_& &STEAM_0:0:84901");
+
+var isScammerTrue = nock("http://steamrep.com")
+                    .get("/id2rep.php?steamID32=STEAM_0:0:21026124")
+                    .reply(200, "_&IS A SCAMMER,SR SCAMMER,SKIAL SCAMMER,FoG SCAMMER,REDDIT SCAMMER &STEAM_0:0:21026124");
 
 describe('SteamRepAPI Wrapper', function () {
 
